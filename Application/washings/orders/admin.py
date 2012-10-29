eafeb111f9c2f687214816ca85debc2e780057d8
Vladimir Ignatyev <vignatyev@flexis.ru>
2012-10-29 16:16:33 +0400

    removed auth, site etc. from admin

--- Application/washings/orders/admin.py
+++ Application/washings/orders/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 from orders.models import Washing, Order, UserProfile
 
 admin.site.register(Washing)
-admin.site.register(Order)
+# admin.site.register(Order)
 admin.site.register(UserProfile)
+
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import Group
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.unregister(Site)
